0942d349530cf817319a6a2c20cf2aebed9a80d2
TomonyteReda <reda.tomonyte@gmail.com>
2022-05-18 21:02:35 +0300

    admin view updates

--- Autoservice/mysite/autoservice/admin.py
+++ Autoservice/mysite/autoservice/admin.py
@@ -2,10 +2,32 @@ from django.contrib import admin
 from .models import CarModel, Car, Service, Order, OrderLine
 
 
+class OrderLineInline(admin.TabularInline):
+    model = OrderLine
+    readonly_fields = ('id',)
+    can_delete = False
+    extra = 0
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('display_service', 'display_quantity', 'display_price', 'order_date', 'car')
+    inlines = [OrderLineInline]
+
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('client', 'car_model', 'country_registration_no', 'vin_code')
+    list_filter = ('client', 'car_model__car_brand', 'car_model__car_model')
+    search_fields = ('country_registration_no', 'vin_code')
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('service_name', 'price')
+
+
 admin.site.register(CarModel)
-admin.site.register(Car)
-admin.site.register(Service)
-admin.site.register(Order)
+admin.site.register(Car, CarAdmin)
+admin.site.register(Service, ServiceAdmin)
+admin.site.register(Order, OrderAdmin)
 admin.site.register(OrderLine)
 
 
--- Autoservice/mysite/autoservice/models.py
+++ Autoservice/mysite/autoservice/models.py
@@ -9,20 +9,28 @@ class CarModel(models.Model):
 
     def __str__(self):
         """String for representing the Model object."""
-        return f'{self.brand} {self.model}'
+        return f'{self.car_brand} {self.car_model}'
+
+    class Meta:
+        verbose_name = 'Car Model'
+        verbose_name_plural = 'Car Models'
 
 
 class Car(models.Model):
     country_registration_no = models.CharField('Country registration number', max_length=100, null=False)
-    car_model = models.ForeignKey(CarModel, on_delete=models.RESTRICT, null=False)
+    car_model = models.ForeignKey(CarModel, on_delete=models.PROTECT, null=False)
     vin_code = models.CharField('VIN', max_length=100)
-    client = models.CharField('Country registration number',
+    client = models.CharField('Client\'s full name',
                               max_length=100,
                               null=False,
                               help_text='Client name and surname')
 
     def __str__(self):
-        return f'{self.country_registration_no} {self.carmodel.car_model} {self.carmodel.car_brand} {self.vin_code}'
+        return f'{self.country_registration_no} {self.car_model} {self.vin_code} {self.client}'
+
+    class Meta:
+        verbose_name = 'Car'
+        verbose_name_plural = 'Cars'
 
     def get_absolute_url(self):
         return reverse('car-detail', args=[str(self.id)])
@@ -36,12 +44,16 @@ class Service(models.Model):
         return reverse('service-detail', args=[str(self.id)])
 
     def __str__(self):
-        return f'{self.service_name} {self.price}'
+        return f'{self.service_name}'
+
+    class Meta:
+        verbose_name = 'Service'
+        verbose_name_plural = 'Services'
 
 
 class Order(models.Model):
     order_date = models.DateField('Order Date', null=False)
-    car = models.ForeignKey(Car, on_delete=models.RESTRICT, null=False)
+    car = models.ForeignKey(Car, on_delete=models.PROTECT, null=False)
     order_amount = models.FloatField('Order Amount', null=False)
 
     class Meta:
@@ -50,12 +62,35 @@ class Order(models.Model):
     def __str__(self):
         return f'{self.id} ({self.order_date}) ({self.car.car_model})'
 
+    class Meta:
+        verbose_name = 'Order'
+        verbose_name_plural = 'Orders'
+
+    def display_service(self):
+        return ', '.join(orderlines.service.service_name for orderlines in self.orderlines.all())
+
+    display_service.short_description = 'Service'
+
+    def display_quantity(self):
+        return ', '.join(str(orderlines.quantity) for orderlines in self.orderlines.all())
+
+    display_quantity.short_description = 'Quantity'
+
+    def display_price(self):
+        return ', '.join(str(orderlines.price) for orderlines in self.orderlines.all())
+
+    display_price.short_description = 'Price'
+
 
 class OrderLine(models.Model):
-    service = models.ForeignKey(Service, on_delete=models.RESTRICT, null=False)
-    order = models.ForeignKey(Order, on_delete=models.RESTRICT, null=False)
+    service = models.ForeignKey(Service, on_delete=models.PROTECT, null=False)
+    order = models.ForeignKey(Order, on_delete=models.PROTECT, null=False, related_name='orderlines')
     quantity = models.IntegerField('Quantity', null=True)
     price = models.FloatField('Price', null=False)
 
     def __str__(self):
-        return f'{self.id} {self.service.service_name} {self.service.quantity} {self.service.price}'
+        return f'{self.id} {self.service.service_name} {self.quantity} {self.price}'
+
+    class Meta:
+        verbose_name = 'Order Line'
+        verbose_name_plural = 'Order Lines'
